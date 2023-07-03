96d86800098e93c69775d291f380edc77c911d0e
Akulav <43496076+Akulav@users.noreply.github.com>
2023-07-03 11:25:27 +0300

    Further project changes
    1. Added gitignore
    2. Added tablelayout
    3. Added pictureBox
    4. Changed Form1 to MainForm

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            MainTable = new TableLayoutPanel();
+            pictureBox = new PictureBox();
+            MainTable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // MainTable
+            // 
+            MainTable.ColumnCount = 1;
+            MainTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            MainTable.Controls.Add(pictureBox, 0, 1);
+            MainTable.Dock = DockStyle.Fill;
+            MainTable.Location = new Point(0, 0);
+            MainTable.Name = "MainTable";
+            MainTable.RowCount = 3;
+            MainTable.RowStyles.Add(new RowStyle(SizeType.Percent, 9.523809F));
+            MainTable.RowStyles.Add(new RowStyle(SizeType.Percent, 76.1904755F));
+            MainTable.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            MainTable.Size = new Size(1256, 550);
+            MainTable.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Image = (Image)resources.GetObject("pictureBox.Image");
+            pictureBox.InitialImage = null;
+            pictureBox.Location = new Point(3, 55);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(1250, 413);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1256, 550);
+            Controls.Add(MainTable);
+            Name = "MainForm";
+            Text = "PhotoViewer - By Akulav & map3x";
+            MainTable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel MainTable;
+        private PictureBox pictureBox;
     }
 }
