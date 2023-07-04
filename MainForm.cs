7d35b5cb6e398a06a5cd1dd16174df404d9755b2
Akulav <43496076+Akulav@users.noreply.github.com>
2023-07-04 05:12:58 +0300

    Added temporary file association

--- MainForm.cs
+++ MainForm.cs
@@ -1,3 +1,7 @@
+using Microsoft.Win32;
+using optimizedPhotoViewer.Extensions;
+using System.Runtime.InteropServices;
+
 namespace optimizedPhotoViewer
 {
     public partial class MainForm : Form
@@ -9,6 +13,10 @@ namespace optimizedPhotoViewer
             {
                 pictureBox.Image = new Bitmap(args);
             }
+            string fullPath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+          
+            FileAssociations.SetAssociation(".png", "optimizedViewer", "Image File", fullPath);
+            FileAssociations.SetAssociation(".jpeg", "optimizedViewer", "Image File", fullPath);
         }
     }
 }
